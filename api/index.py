bbd92727f82d8c6def7135f1109b2ca8ac1a6862
Tenacs <62227093+Tenacs@users.noreply.github.com>
2024-09-27 18:09:43 -0400

    add database

--- api/index.py
+++ api/index.py
@@ -1,6 +1,40 @@
 from flask import Flask
+import sqlite3
 app = Flask(__name__)
 
-@app.route("/api/python")
-def hello_world():
-    return "<p>Hello, World!</p>"
+
+@app.route("/api/awards")
+def search_awards():
+    with sqlite3.connect("api/Awards.db") as connection:
+        cursor = connection.cursor()
+        awards = cursor.execute("SELECT * FROM awards").fetchall()
+
+    return f"<p>Awards: {awards}</p>"
+
+
+
+@app.route("/api/addAward")
+def add_award():
+    with sqlite3.connect("api/Awards.db") as connection:
+        cursor = connection.cursor()
+        cursor.execute("CREATE TABLE IF NOT EXISTS awards (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, abstract TEXT, investigator TEXT, amount INTEGER, date TEXT)")
+        
+        cursor.execute(
+            "INSERT INTO awards (title, abstract, investigator, amount, date) VALUES (?, ?, ?, ?, ?)",
+            ("award title", "an abstract", "John Doe", 265000, "2024-01-01")
+        )
+
+        awards = cursor.execute("SELECT * FROM awards").fetchall()
+
+    return f"<p>Awards: {awards}</p>"
+
+
+
+@app.route("/api/deleteAwards")
+def delete_awards():
+    with sqlite3.connect("api/Awards.db") as connection:
+        cursor = connection.cursor()
+        cursor.execute("DROP TABLE awards")
+
+    return "<p>Table dropped</p>"
+    
