cfcab437ca3151cb6b48960dd340d34219e48395
Acrisp2002 <97912824+Acrisp2002@users.noreply.github.com>
2024-10-10 20:13:06 -0400

    Update index.py
    Added search queries for Investigator Name and Organization Abbreviation

--- api/index.py
+++ api/index.py
@@ -1,24 +1,54 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import sqlite3
+import pandas as pd
 app = Flask(__name__)
 
 
-@app.route("/api/awards")
-def search_awards():
-    with sqlite3.connect("api/Awards.db") as connection:
-        cursor = connection.cursor()
-        awards = cursor.execute("SELECT * FROM awards LIMIT 20").fetchall()
+# Database connection
+def get_db_connection():
+    conn = sqlite3.connect('api/Awards.db')
+    return conn
 
-        htmlDiv = ''
+def query_to_json(query, params=()):
+    conn = get_db_connection()
+    df = pd.read_sql_query(query, conn, params=params)
+    conn.close()
+    return df.to_json(orient='records')
 
-        for award in awards:
-            htmlDiv += f"<div>Award ID: {award[0]}</div>"
-            htmlDiv += f"<div>Award Title: {award[1]}</div>"
-            htmlDiv += f"<div>Award Amount: ${award[5]}</div>"
-            htmlDiv += f"<div>Institution: {award[8]}</div>"
-            htmlDiv += f"<br/><br/><br/>"
+# Search by Award Title
+@app.route('/search_award_title', methods=['GET'])
+def search_by_award_title():
+    title = request.args.get('title')
+    query = "SELECT * FROM awards WHERE title LIKE ?"
+    return query_to_json(query, (f"%{title}%",))
 
-        return htmlDiv
+# Search by Award ID
+@app.route('/search_award_id', methods=['GET'])
+def search_by_award_id():
+    award_id = request.args.get('award_id')
+    query = "SELECT * FROM awards WHERE awardID = ?"
+    return query_to_json(query, (award_id,))
 
+# Search by Organization/University
+@app.route('/search_institution', methods=['GET'])
+def search_by_institution():
+    institution_name = request.args.get('institution')
+    query = "SELECT * FROM awards WHERE institution LIKE ?"
+    return query_to_json(query, (f"%{institution_name}%",))
 
-    
+# Search by Investigator Name
+@app.route('/search_name', methods=['GET'])
+def search_by_name():
+    investigator_name = request.args.get('name')
+    query = "SELECT * FROM investigators WHERE name LIKE ?"
+    return query_to_json(query, (f"%{investigator_name}%",))
+
+#Search by Organization Abbreviation
+@app.route('/search_org_abbr', methods=['GET'])
+def search_by_org_abbr():
+    org_abbr = request.args.get('org_abbr')
+    query = "SELECT * FROM awards WHERE org_abbr LIKE ?"
+    return query_to_json(query, (f"%{org_abbr}%",))
+
+if __name__ == '__main__':
+    app.run(debug=True)
