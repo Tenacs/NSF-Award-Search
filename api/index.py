b9d996c0237dda4d80a1bb77f8b00e95338178f9
Tenacs <62227093+Tenacs@users.noreply.github.com>
2024-10-12 11:03:53 -0400

    removed polars

--- api/index.py
+++ api/index.py
@@ -1,6 +1,4 @@
-from flask import Flask
 import sqlite3
-import polars as pl
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -11,52 +9,61 @@ def get_db_connection():
     conn = sqlite3.connect('api/Awards.db')
     return conn
 
-def query_to_json(query, params=()):
-    conn = get_db_connection()
-    
-    df = pl.read_database(
-        query=query,
-        connection=conn,
-        execute_options={"parameters": params},
-        )
-
-    conn.close()
-    return df.write_json()
 
 # Search by Award Title
 @app.route('/api/search_award_title', methods=['GET'])
 def search_by_award_title():
+    conn = get_db_connection()
+    cursor = conn.cursor()
     title = request.args.get('title')
-    query = "SELECT * FROM awards WHERE title LIKE ?"
-    return query_to_json(query, (f"%{title}%",))
+    results = cursor.execute("SELECT * FROM awards WHERE title LIKE ?", (f"%{title}%",)).fetchall()
+    conn.close()
+
+    return results
 
 # Search by Award ID
 @app.route('/api/search_award_id', methods=['GET'])
 def search_by_award_id():
+    conn = get_db_connection()
+    cursor = conn.cursor()
     award_id = request.args.get('award_id')
-    query = "SELECT * FROM awards WHERE awardID = ?"
-    return query_to_json(query, (award_id,))
+    results = cursor.execute("SELECT * FROM awards WHERE awardID = ?", (award_id,)).fetchall()
+    conn.close()
+
+    return results
 
 # Search by Organization/University
 @app.route('/api/search_institution', methods=['GET'])
 def search_by_institution():
+    conn = get_db_connection()
+    cursor = conn.cursor()
     institution_name = request.args.get('institution')
-    query = "SELECT * FROM awards WHERE institution LIKE ?"
-    return query_to_json(query, (f"%{institution_name}%",))
+    results = cursor.execute("SELECT * FROM awards WHERE institution LIKE ?", (f"%{institution_name}%",)).fetchall()
+    conn.close()
+
+    return results
 
 # Search by Investigator Name
 @app.route('/api/search_name', methods=['GET'])
 def search_by_name():
+    conn = get_db_connection()
+    cursor = conn.cursor()
     investigator_name = request.args.get('name')
-    query = "SELECT * FROM investigators WHERE name LIKE ?"
-    return query_to_json(query, (f"%{investigator_name}%",))
+    results = cursor.execute("SELECT * FROM investigators WHERE name LIKE ?", (f"%{investigator_name}%",)).fetchall()
+    conn.close()
+
+    return results
 
 #Search by Organization Abbreviation
 @app.route('/api/search_org_abbr', methods=['GET'])
 def search_by_org_abbr():
+    conn = get_db_connection()
+    cursor = conn.cursor()
     org_abbr = request.args.get('org_abbr')
-    query = "SELECT * FROM awards WHERE org_abbr LIKE ?"
-    return query_to_json(query, (f"%{org_abbr}%",))
+    results = cursor.execute("SELECT * FROM awards WHERE org_abbr LIKE ?", (f"%{org_abbr}%",)).fetchall()
+    conn.close()
+
+    return results
 
 if __name__ == '__main__':
     app.run(debug=True)
